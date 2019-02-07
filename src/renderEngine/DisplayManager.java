a202fe60723e1c8ff7a07ea4babf23d48b111ff2
aronthompson <aronthompsonemail@gmail.com>
2019-02-08 00:30:19 +0100

    2: VAOs and VBOs

--- src/renderEngine/DisplayManager.java
+++ src/renderEngine/DisplayManager.java
@@ -1,5 +1,42 @@
 package renderEngine;
 
-public class DisplayManager {
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.ContextAttribs;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.opengl.PixelFormat;
 
+public class DisplayManager {
+	private static final int WIDTH = 1280;
+	private static final int HEIGHT = 720;
+	private static final int FPS_CAP = 120;
+	
+	public static void createDisplay() {
+		ContextAttribs attribs = new ContextAttribs(3,2);
+		attribs.withForwardCompatible(true);
+		attribs.withProfileCore(true);
+		
+		try {
+			Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
+			Display.create(new PixelFormat(), attribs);
+			Display.setTitle("Our First Display");
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+		}
+		
+		// where it starts displaying and size
+		GL11.glViewport(0, 0, WIDTH, HEIGHT);
+		
+	}
+	
+	public static void udpateDisplay() {
+		// handling fps
+		Display.sync(FPS_CAP);
+		Display.update();
+	}
+	
+	public static void closeDisplay () {
+		Display.destroy();
+	}
 }
